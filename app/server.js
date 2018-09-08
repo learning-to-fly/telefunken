51aa72720c3b9782c7df61e7e426e7ad62154af1
ludnas <ksandul@gmail.com>
2018-09-09 01:02:46 +0300

    server.js

--- app/server.js
+++ app/server.js
@@ -1,1 +1,9 @@
-// Start
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static(path.join('reactapp', 'build')));
+
+app.listen(3000, function(){
+    console.log("Magic happens now at port 3000");
+});
